c42a14a5b8423ab1642032065782d9525e6acd8a
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-10-15 10:54:18 +0400

    bugfix(): resolve IndexError: list index out of range [while running ParseRawData]

--- local_pipelines/sample101.py
+++ local_pipelines/sample101.py
@@ -1,44 +1,54 @@
 import logging
 
+import apache_beam as beam
 import click
+from apache_beam.options.pipeline_options import PipelineOptions
 
 logging.basicConfig(level=logging.INFO)
 
 
-def transform_raw_txt_to_dict(raw_txt):
-    transformed_dicts = []
+class ParseRawData(beam.DoFn):
+    def process(self, lines):
+        transformed_dicts = []
+        logging.info(f'Lines received: {lines}')
+        logging.info(f'Type of Lines received: {type(lines)}')
+        lines = [lines] if not isinstance(lines, list) else lines
 
-    for line in raw_txt:
-        parts = line.split(',') if ',' in line else None
-        if not parts:
-            logging.info(f'Malformed text line detected: {line}')
-            continue
+        for line in lines:
+            parts = line.split(',') if ',' in line else None
 
-        _dict = {}
-        dict_parts = [x.split(':') for x in parts]
+            if not parts:
+                logging.info(f'Malformed text line detected: {line}')
+                continue
 
-        for key in dict_parts:
-            _dict[key[0].strip()] = key[1].strip()
+            _dict = {}
+            dict_parts = [x.split(':') for x in parts]
 
-        transformed_dicts.append(_dict)
+            for key in dict_parts:
+                _dict[key[0].strip()] = key[1].strip()
 
-    logging.info(f'Final result: {transformed_dicts}')
+            transformed_dicts.append(_dict)
 
-    return transformed_dicts
+        logging.info(f'Final result: {transformed_dicts}')
+
+        return transformed_dicts
 
 
 @click.command()
 @click.option('--f_in', help='Input file')
 @click.option('--f_out', required=False, help='Output file')
 def main(f_in, f_out):
-    logging.info(f'Input file extracted, {f_in}!')
+    logging.info(f'Input file extracted: {f_in}!')
 
-    with open(f_in) as f:
-        data = f.readlines()
-        logging.info(f'Data extracted: {data}')
-        data = transform_raw_txt_to_dict(raw_txt=data)
+    with beam.Pipeline('DirectRunner', options=PipelineOptions()) as p:
+        # Create a PCollection from the input file.
+        lines = p | 'ReadFromText' >> beam.io.ReadFromText(f_in)
 
-    return data
+        # Apply a ParDo transform to parse each line into valid dicts.
+        dicts = lines | 'ParseRawData' >> beam.ParDo(ParseRawData())
+
+        # Write the dicts to a file.
+        dicts | 'WriteToText' >> beam.io.WriteToText(f_out)
 
 
 if __name__ == '__main__':
